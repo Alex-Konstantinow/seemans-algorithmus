44bdf418577e810be08f35af9f19e9d0fd0f3c18
Lixiang Wu <liwu@mail.uni-mannheim.de>
2017-09-18 00:32:19 +0200

    Aufgabe 4 finished + Bonus

--- src/main/RRSolver.java
+++ src/main/RRSolver.java
@@ -26,16 +26,30 @@ public class RRSolver {
 	 * Tiefensuche und gibt diese als String zurueck
 	 */
 	private static String solve(int n) throws Exception {
+		ArrayList<String> counter = new ArrayList();
 		RRState robin = new RRState(n);
 		Stack stapel = new Stack();
 		stapel.push(robin);
 		do {
 			if (stapel.isEmpty() == true) {
-				return "Keine Lösung";
+//				return "Keine Lösung";
+				for(Iterator i = counter.iterator(); i.hasNext();){
+					System.out.println(i.next().toString());
+				}
+				return counter.size() + " Lösungen";
 			} else {
 				RRState state = (RRState)stapel.pop();
 				if (state.isSolution() == true) {
-					return state.toString();
+//					return state.toString();
+					boolean flag = false;
+					for(Iterator i = counter.iterator(); i.hasNext();){
+						if(i.next().toString().equals(state.toString())){
+							flag = true;
+						}
+					}
+					if(flag == false){
+						counter.add(state.toString());
+					}
 				} else {
 					for (Iterator i = state.expand().iterator(); i.hasNext(); ) {
 						stapel.push((RRState)i.next());
